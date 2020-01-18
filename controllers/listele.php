7941e5e1f23609ae10b8f1382f16e12c42bc313c
huseyintekn <60022384+huseyintekn@users.noreply.github.com>
2020-01-18 05:03:22 +0300

    listele.php
    kayıtları listele

---
+++ controllers/listele.php
@@ -0,0 +1,17 @@
+<?php 
+class listele extend Controller
+{
+    public function __construct()
+    {
+      parent::__construct()
+      
+      $this->Modelyukle('listele');
+    
+    ]
+  
+
+}
+
+
+
+?>
